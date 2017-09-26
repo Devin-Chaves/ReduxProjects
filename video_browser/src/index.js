e8fb9865acdc50255c3fe6091949fb4f7bd47b53
Devin Chaves <dchav90@gmail.com>
2017-09-26 18:27:57 -0400

    Refactor App from func to class

--- video_browser/src/index.js
+++ video_browser/src/index.js
@@ -1,22 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyDdAMe3UQOTzPVr5T7SzVz35Oedpca9keY';
 
-YTSearch({ key: API_KEY, term: 'surfboards' }, function(data) {
-  console.log(data);
-});
 // Create a new component - should produce some HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { videos: [] };
+    YTSearch({ key: API_KEY, term: 'surfboards' }, videos => {
+      this.setState({ videos });
+    });
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
 
 // Take this component's generated HTML and insert
 // into DOM
