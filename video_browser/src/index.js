7cdea4bf1ad53bdd9ed15f81607658958de811c6
Devin Chaves <dchav90@gmail.com>
2017-09-26 18:20:57 -0400

    Received response from YTSearch

--- video_browser/src/index.js
+++ video_browser/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// Create a new component - should produce some HTML
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyDdAMe3UQOTzPVr5T7SzVz35Oedpca9keY';
 
+YTSearch({ key: API_KEY, term: 'surfboards' }, function(data) {
+  console.log(data);
+});
+// Create a new component - should produce some HTML
 const App = () => {
   return (
     <div>
