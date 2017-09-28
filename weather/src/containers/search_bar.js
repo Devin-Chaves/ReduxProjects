c074233ffc158bd499babaf4072b3b6ae77e9daa
Devin Chaves <dchav90@gmail.com>
2017-09-28 15:09:46 -0400

    Issue ajax request and clear field

--- weather/src/containers/search_bar.js
+++ weather/src/containers/search_bar.js
@@ -9,6 +9,7 @@ class SearchBar extends Component {
 
     this.state = { term: '' };
     this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
   }
   onInputChange(event) {
     this.setState({ term: event.target.value });
@@ -17,6 +18,8 @@ class SearchBar extends Component {
     event.preventDefault();
 
     // We need to go and fetch weather data
+    this.props.fetchWeather(this.state.term);
+    this.setState({ term: '' });
   }
   render() {
     return (
