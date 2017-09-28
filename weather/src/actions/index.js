992151bbe437c83b31b90e7ea775cfc4c1f3c9b6
Devin Chaves <dchav90@gmail.com>
2017-09-28 13:46:50 -0400

    import and request with axios

--- weather/src/actions/index.js
+++ weather/src/actions/index.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 const API_KEY = '9c95d8b5fa8b8a9030bf0db2af1aefe0';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
@@ -7,7 +8,10 @@ export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
   return {
-    type: FETCH_WEATHER
+    type: FETCH_WEATHER,
+    payload: request
   };
 }
