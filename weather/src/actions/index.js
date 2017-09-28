48830bf8377b6b9cabab6bc072c9afc33ec60e53
Devin Chaves <dchav90@gmail.com>
2017-09-28 13:44:08 -0400

    Weather URL query string

--- weather/src/actions/index.js
+++ weather/src/actions/index.js
@@ -1,1 +1,13 @@
 const API_KEY = '9c95d8b5fa8b8a9030bf0db2af1aefe0';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+// API Request responsible for fetching weather data
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  return {
+    type: FETCH_WEATHER
+  };
+}
