f60070fa49cba7bd803ca7ce5b90f57502c429fc
Devin Chaves <dchav90@gmail.com>
2017-09-30 20:19:19 -0400

    GoogleMap component

---
+++ weather/src/components/google_map.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class GoogleMap extends Component {
+  render() {
+    // this.refs.map
+    return <div ref="map" />;
+  }
+}
+
+export default GoogleMap;
