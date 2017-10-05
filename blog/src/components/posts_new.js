df34eac913dfb0341895e3416c82db5fc14f97d5
Devin Chaves <dchav90@gmail.com>
2017-10-05 13:38:37 -0400

    tags > categories*

--- blog/src/components/posts_new.js
+++ blog/src/components/posts_new.js
@@ -14,7 +14,11 @@ class PostsNew extends Component {
     return (
       <form>
         <Field label="Title" name="title" component={this.renderField} />
-        <Field label="Tags" name="tags" component={this.renderField} />
+        <Field
+          label="Categories"
+          name="categories"
+          component={this.renderField}
+        />
         <Field
           label="Post Content"
           name="content"
