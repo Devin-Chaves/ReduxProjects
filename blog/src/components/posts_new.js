ec73466c79626503805968cfbd44dc71fab55f64
Devin Chaves <dchav90@gmail.com>
2017-10-05 13:53:21 -0400

    Validation and error reporting

--- blog/src/components/posts_new.js
+++ blog/src/components/posts_new.js
@@ -7,6 +7,7 @@ class PostsNew extends Component {
       <div className="form-group">
         <label>{field.label}</label>
         <input type="text" className="form-control" {...field.input} />
+        {field.meta.error}
       </div>
     );
   }
@@ -29,6 +30,26 @@ class PostsNew extends Component {
   }
 }
 
+function validate(values) {
+  const errors = {};
+
+  // Validate inputs from values 'values'
+  if (!values.title || values.title.length < 3) {
+    errors.title = 'Enter a title that is at least 3 characters';
+  }
+  if (!values.categories) {
+    errors.title = 'Enter at least 1 category!';
+  }
+  if (!values.content) {
+    errors.title = 'Enter some content!';
+  }
+  // If errors the for is fine to submit
+  // If errors has any properties, reduxForm assumes form is invalid
+  return errors;
+}
+
 export default reduxForm({
+  // validate: validate
+  validate,
   form: 'PostsNewForm'
 })(PostsNew);
